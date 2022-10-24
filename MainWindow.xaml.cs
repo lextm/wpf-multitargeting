7d706c04f145d1f385ebbe92933d9b0f5ab92234
Lex Li <support@lextm.com>
2022-10-24 15:28:03 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUWP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+#if WINDOWS10_0_17763_0_OR_GREATER
+            new ToastContentBuilder()
+                 .AddArgument("action", "viewConversation")
+                 .AddArgument("conversationId", 9813)
+                 .AddText("Andrew sent you a picture")
+                 .AddText("Check this out, The Enchantments in Washington!")
+                 .Show();
+#endif
+        }
+    }
+}
